c10a577e11b63f51350f2911aeb54a4691850096
zhiqiangxu <652732310@qq.com>
2025-09-27 00:32:35 +0800

    fix typo (#43)

--- sbpf/src/memory_region.rs
+++ sbpf/src/memory_region.rs
@@ -14,7 +14,7 @@ use std::{array, cell::UnsafeCell, fmt, mem, ops::Range, ptr};
     The MemoryMapping supports a special mapping mode which is used for the stack MemoryRegion.
     In this mode the backing address space of the host is sliced in power-of-two aligned frames.
     The exponent of this alignment is specified in vm_gap_shift. Then the virtual address space
-    of the guest is spread out in a way which leaves gapes, the same size as the frames, in
+    of the guest is spread out in a way which leaves gaps, the same size as the frames, in
     between the frames. This effectively doubles the size of the guests virtual address space.
     But the actual mapped memory stays the same, as the gaps are not mapped and accessing them
     results in an AccessViolation.
